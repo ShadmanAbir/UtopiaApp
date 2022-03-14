9d16a2536c36a6b0c06bdcd79cafa727190f3bb5
Shadman Sakib <shadmanabir@outlook.com>
2022-03-13 22:22:19 -0700

    init

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace UtopiaApp
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.Menu = new System.Windows.Forms.Panel();
+            this.SalesButton = new System.Windows.Forms.Button();
+            this.SalesPanel = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxDue = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxBkash = new System.Windows.Forms.TextBox();
+            this.textBoxBank = new System.Windows.Forms.TextBox();
+            this.textBoxCash = new System.Windows.Forms.TextBox();
+            this.comboBoxMonth = new System.Windows.Forms.ComboBox();
+            this.BtnMNY = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxYear = new System.Windows.Forms.TextBox();
+            this.ShowButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.ViewPanel = new System.Windows.Forms.Panel();
+            this.Menu.SuspendLayout();
+            this.SalesPanel.SuspendLayout();
+            this.BtnMNY.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Menu
+            // 
+            this.Menu.Controls.Add(this.SalesButton);
+            this.Menu.Location = new System.Drawing.Point(0, 0);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(232, 604);
+            this.Menu.TabIndex = 0;
+            // 
+            // SalesButton
+            // 
+            this.SalesButton.Location = new System.Drawing.Point(61, 187);
+            this.SalesButton.Name = "SalesButton";
+            this.SalesButton.Size = new System.Drawing.Size(94, 29);
+            this.SalesButton.TabIndex = 0;
+            this.SalesButton.Text = "Sales";
+            this.SalesButton.UseVisualStyleBackColor = true;
+            // 
+            // SalesPanel
+            // 
+            this.SalesPanel.Controls.Add(this.label4);
+            this.SalesPanel.Controls.Add(this.textBoxDue);
+            this.SalesPanel.Controls.Add(this.label3);
+            this.SalesPanel.Controls.Add(this.label2);
+            this.SalesPanel.Controls.Add(this.label1);
+            this.SalesPanel.Controls.Add(this.textBoxBkash);
+            this.SalesPanel.Controls.Add(this.textBoxBank);
+            this.SalesPanel.Controls.Add(this.textBoxCash);
+            this.SalesPanel.Location = new System.Drawing.Point(238, 0);
+            this.SalesPanel.Name = "SalesPanel";
+            this.SalesPanel.Size = new System.Drawing.Size(421, 216);
+            this.SalesPanel.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(75, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Due";
+            // 
+            // textBoxDue
+            // 
+            this.textBoxDue.Location = new System.Drawing.Point(244, 144);
+            this.textBoxDue.Name = "textBoxDue";
+            this.textBoxDue.Size = new System.Drawing.Size(125, 27);
+            this.textBoxDue.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Bkash";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Bank:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Cash:";
+            // 
+            // textBoxBkash
+            // 
+            this.textBoxBkash.Location = new System.Drawing.Point(244, 109);
+            this.textBoxBkash.Name = "textBoxBkash";
+            this.textBoxBkash.Size = new System.Drawing.Size(125, 27);
+            this.textBoxBkash.TabIndex = 4;
+            // 
+            // textBoxBank
+            // 
+            this.textBoxBank.Location = new System.Drawing.Point(244, 76);
+            this.textBoxBank.Name = "textBoxBank";
+            this.textBoxBank.Size = new System.Drawing.Size(125, 27);
+            this.textBoxBank.TabIndex = 3;
+            // 
+            // textBoxCash
+            // 
+            this.textBoxCash.Location = new System.Drawing.Point(244, 43);
+            this.textBoxCash.Name = "textBoxCash";
+            this.textBoxCash.Size = new System.Drawing.Size(125, 27);
+            this.textBoxCash.TabIndex = 2;
+            // 
+            // comboBoxMonth
+            // 
+            this.comboBoxMonth.FormattingEnabled = true;
+            this.comboBoxMonth.Location = new System.Drawing.Point(186, 63);
+            this.comboBoxMonth.Name = "comboBoxMonth";
+            this.comboBoxMonth.Size = new System.Drawing.Size(151, 28);
+            this.comboBoxMonth.TabIndex = 0;
+            // 
+            // BtnMNY
+            // 
+            this.BtnMNY.Controls.Add(this.label6);
+            this.BtnMNY.Controls.Add(this.label5);
+            this.BtnMNY.Controls.Add(this.textBoxYear);
+            this.BtnMNY.Controls.Add(this.ShowButton);
+            this.BtnMNY.Controls.Add(this.SaveButton);
+            this.BtnMNY.Controls.Add(this.comboBoxMonth);
+            this.BtnMNY.Location = new System.Drawing.Point(665, 0);
+            this.BtnMNY.Name = "BtnMNY";
+            this.BtnMNY.Size = new System.Drawing.Size(382, 216);
+            this.BtnMNY.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(55, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Year";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Month";
+            // 
+            // textBoxYear
+            // 
+            this.textBoxYear.Location = new System.Drawing.Point(186, 109);
+            this.textBoxYear.MaxLength = 4;
+            this.textBoxYear.Name = "textBoxYear";
+            this.textBoxYear.PlaceholderText = "Year";
+            this.textBoxYear.Size = new System.Drawing.Size(151, 27);
+            this.textBoxYear.TabIndex = 4;
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(224, 161);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(94, 29);
+            this.ShowButton.TabIndex = 3;
+            this.ShowButton.Text = "Show";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(55, 161);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(94, 29);
+            this.SaveButton.TabIndex = 2;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // ViewPanel
+            // 
+            this.ViewPanel.Location = new System.Drawing.Point(240, 222);
+            this.ViewPanel.Name = "ViewPanel";
+            this.ViewPanel.Size = new System.Drawing.Size(807, 382);
+            this.ViewPanel.TabIndex = 3;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 605);
+            this.Controls.Add(this.ViewPanel);
+            this.Controls.Add(this.BtnMNY);
+            this.Controls.Add(this.SalesPanel);
+            this.Controls.Add(this.Menu);
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
+            this.Menu.ResumeLayout(false);
+            this.SalesPanel.ResumeLayout(false);
+            this.SalesPanel.PerformLayout();
+            this.BtnMNY.ResumeLayout(false);
+            this.BtnMNY.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel Menu;
+        private Button SalesButton;
+        private Panel SalesPanel;
+        private Label label1;
+        private TextBox textBoxBkash;
+        private TextBox textBoxBank;
+        private TextBox textBoxCash;
+        private ComboBox comboBoxMonth;
+        private Label label3;
+        private Label label2;
+        private Label label4;
+        private TextBox textBoxDue;
+        private Panel BtnMNY;
+        private Button SaveButton;
+        private Button ShowButton;
+        private Panel ViewPanel;
+        private TextBox textBoxYear;
+        private Label label5;
+        private Label label6;
+    }
+}
